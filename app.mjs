1d45b33b6a2a2c86fe8ac09e9a84dfe5ed4329a1
Syed Usman <mr.usman.ali141@gmail.com>
2023-05-18 20:56:07 +0500

    view code

---
+++ app.mjs
@@ -0,0 +1,57 @@
+let percentage = prompt('Enter Your Percentage');
+
+if (percentage > 100){
+    alert ('Please enter a valid Percentage')
+
+    console.log ('Please enter a valid Percentage')
+}
+
+else if (percentage >= 80) 
+{
+    alert ('Your Grade Is A+')
+
+    console.log ('Your Grade Is A+')
+}
+
+else if (percentage>=70) 
+{
+    alert ('Your Grade Is A')
+
+    console.log ('Your Grade Is A')
+}
+
+else if (percentage>=60) {
+    alert ('Your Grade Is B')
+
+    console.log ('Your Grade Is B')
+}
+
+else if (percentage>=50) {
+    alert ('Your Grade Is C')
+
+    console.log ('Your Grade Is C')
+}
+
+else if (percentage>=40) {
+    alert ('Your Grade Is D')
+
+    console.log ('Your Grade Is D')
+}
+
+else if (percentage>=33) {
+    alert ('Your Grade Is E')
+
+    console.log ('Your Grade Is E')
+}
+
+else if (percentage>=0) {
+    alert ('Sorry, You Are Failed')
+
+    console.log ('Sorry, You Are Failed')
+}
+
+else {
+    alert ('Please enter a valid Percentage')
+
+    console.log ('Please enter a valid Percentage')
+}
